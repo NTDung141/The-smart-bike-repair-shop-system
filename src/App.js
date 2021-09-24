944ce66da38ce0e7ded2f80aab48611e2d34248d
Nguyen Thanh Dung <dungynot@gmail.com>
2021-09-24 16:43:27 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Map from './components/map/Map';
 
 function App() {
+  const key = 'AIzaSyAdJlv40A2sRw9m9khaxgFkkDxgM5SjAmM'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Map
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${key}&callback=initMap`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100vh`, margin: `auto`, border: '2px solid black' }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
     </div>
   );
 }
